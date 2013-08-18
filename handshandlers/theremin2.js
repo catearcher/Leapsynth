b7e5beef2060ab9a96014bdc493e161fb9a2e3fb
catearcher <indieboy86@gmail.com>
2013-08-18 18:55:28 +0200

    Stub für Theremin 2.0

---
+++ handshandlers/theremin2.js
@@ -0,0 +1,56 @@
+window.LSHandsHandlers = window.LSHandsHandlers || {};
+
+LSHandsHandlers.theremin2 = {
+  name: "Theremin 2.0",
+  color: "success",
+  handleHands: function(hands, context) {
+    var $handPosition, $fingers, hand, distance, amplitude, frequency;
+
+    _.each(["left", "right"], function(which) {
+      hand = hands[which];
+
+      $handPosition = $("#" + which + "-hand-position");
+      $fingers = $("#" + which + "-hand-fingers");
+
+      if (hand) {
+        $handPosition.html("Position der Hand: " + hand.position.join(" / "));
+        $fingers.html("Anzahl der Finger: " + hand.fingers);
+
+        distance = hand.position[1];
+
+        if (which === "left") {
+          amplitude = 4 - (distance / 100);
+          amplitude = Math.round(10*amplitude) / 10;
+        } else {
+          frequency = 1000 - distance;
+          frequency = Math.round(frequency);
+        }
+      } else {
+        $handPosition.empty();
+        $fingers.empty();
+      }
+    });
+
+    if (hands.left && hands.right) {
+      if (hands.left.fingers >= 2) {
+        if (amplitude !== context.currentAmplitude) {
+          context.setAmplitude(amplitude);
+        }
+      } else {
+        context.setAmplitude(0);
+      }
+
+      if (frequency !== context.currentFrequency) {
+        context.noteOn(frequency);
+      }
+
+      $("#note-frequency").html("Frequenz: " + frequency + "Hz");
+      $("#note-amplitude").html("Amplitude: " + amplitude);
+    } else {
+      context.noteOff();
+
+      $("#note-frequency").empty();
+      $("#note-amplitude").empty();
+    }
+  }
+};
