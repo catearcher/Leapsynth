63ac003a21cb5eba61c5f407157a33f64abf2102
catearcher <indieboy86@gmail.com>
2013-08-19 10:31:00 +0200

    T-Pain: Notenwerte anzeigen

--- handshandlers/tpain.js
+++ handshandlers/tpain.js
@@ -6,59 +6,60 @@ LSHandsHandlers.tpain = {
   handleHands: function(hands, audio) {
     var
     $handPosition, $fingers, hand, distance, amplitude, frequency,
+    frequencyDescription = "",
     noteFrequencies = [
-      16.35,
-      18.35,
-      20.60,
-      21.83,
-      24.50,
-      27.50,
-      30.87,
-      32.70,
-      36.71,
-      41.20,
-      43.65,
-      49.00,
-      55.00,
-      61.74,
-      65.41,
-      73.42,
-      82.41,
-      87.31,
-      98.00,
-      110.00,
-      123.47,
-      130.81,
-      146.83,
-      164.81,
-      174.61,
-      196.00,
-      220.00,
-      246.94,
-      261.63,
-      293.66,
-      329.63,
-      349.23,
-      392.00,
-      440.00,
-      493.88,
-      523.25,
-      587.33,
-      659.26,
-      698.46,
-      783.99,
-      880.00,
-      987.77,
-      1046.50,
-      1174.66,
-      1318.51,
-      1396.91,
-      1567.98,
-      1760.00,
-      1975.53,
-      2093.00,
-      2349.32,
-      2637.02
+      ["C0", 16.35],
+      ["D0", 18.35],
+      ["E0", 20.60],
+      ["F0", 21.83],
+      ["G0", 24.50],
+      ["A0", 27.50],
+      ["H0", 30.87],
+      ["C1", 32.70],
+      ["D1", 36.71],
+      ["E1", 41.20],
+      ["F1", 43.65],
+      ["G1", 49.00],
+      ["A1", 55.00],
+      ["H1", 61.74],
+      ["C2", 65.41],
+      ["D2", 73.42],
+      ["E2", 82.41],
+      ["F2", 87.31],
+      ["G2", 98.00],
+      ["A2", 110.00],
+      ["H2", 123.47],
+      ["C3", 130.81],
+      ["D3", 146.83],
+      ["E3", 164.81],
+      ["F3", 174.61],
+      ["G3", 196.00],
+      ["A3", 220.00],
+      ["H3", 246.94],
+      ["C4", 261.63],
+      ["D4", 293.66],
+      ["E4", 329.63],
+      ["F4", 349.23],
+      ["G4", 392.00],
+      ["A4", 440.00],
+      ["H4", 493.88],
+      ["C5", 523.25],
+      ["D5", 587.33],
+      ["E5", 659.26],
+      ["F5", 698.46],
+      ["G5", 783.99],
+      ["A5", 880.00],
+      ["H5", 987.77],
+      ["C6", 1046.50],
+      ["D6", 1174.66],
+      ["E6", 1318.51],
+      ["F6", 1396.91],
+      ["G6", 1567.98],
+      ["A6", 1760.00],
+      ["H6", 1975.53],
+      ["C7", 2093.00],
+      ["D7", 2349.32],
+      ["E7", 2637.02]
     ];
 
     _.each(["left", "right"], function(which) {
@@ -83,11 +84,13 @@ LSHandsHandlers.tpain = {
           frequency = Math.round(Math.pow(2, (frequency / 20 - 69) / 12) * 440);
 
           for (i = 0; i < noteFrequencies.length; i++) {
-            if (noteFrequencies[i] > frequency) {
-              if (i > 0 && noteFrequencies[i] - frequency > frequency - noteFrequencies[i-1]) {
-                frequency = noteFrequencies[i-1];
+            if (noteFrequencies[i][1] > frequency) {
+              if (i > 0 && noteFrequencies[i][1] - frequency > frequency - noteFrequencies[i-1][1]) {
+                frequency = noteFrequencies[i-1][1];
+                frequencyDescription = "(" + noteFrequencies[i-1][0] + ")";
               } else {
-                frequency = noteFrequencies[i];
+                frequency = noteFrequencies[i][1];
+                frequencyDescription = "(" + noteFrequencies[i][0] + ")";
               }
 
               break;
@@ -113,7 +116,7 @@ LSHandsHandlers.tpain = {
         audio.setAmplitude(amplitude);
       }
 
-      $("#note-frequency").html("Frequenz: " + frequency + "Hz");
+      $("#note-frequency").html("Frequenz: " + frequency + "Hz " + frequencyDescription);
       $("#note-amplitude").html("Amplitude: " + amplitude);
     } else {
       audio.silence();
